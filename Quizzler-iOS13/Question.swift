7d46474eae8f1932d422b2864772f3763f9ba9c9
Bokhulenkov <bokhulenkov@gmail.com>
2024-06-03 13:20:52 +0300

    create Struct for question

---
+++ Quizzler-iOS13/Question.swift
@@ -0,0 +1,13 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Alexander Bokhulenkov on 03.06.2024.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    
+}
--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    let quiz = [
+    ["Страна с самой высокой продолжительностью жизни - Китай", "True"],
+    ["В щи по старым русским рецептам добавляли простоквашу", "True"],
+    ["Большой зайцегуб – это летучая мышь", "True" ],
+    ["Местожительства волка – это нора", "False"]
+    ]
+    
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        createQuestionLabel()
     }
 
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle
+        let actualAnswer = quiz[questionNumber][1]
+        
+        if userAnswer == actualAnswer {
+            print("Right!")
+        } else {
+            print("Wrong!")
+        }
+        
+        if questionNumber < (quiz.count-1) {
+            questionNumber += 1
+        } else {
+            questionNumber = 0
+        }
+        createQuestionLabel()
+    }
+    
+    private func createQuestionLabel() {
+        questionLabel.text = quiz[questionNumber][0]
+        
+    }
+    
 
 }
 
